705ba569d4a3e5f5b25622185351fe32efe7b047
Keematics <agbajeabdulakeems@gmail.com>
2024-08-12 15:43:21 +0100

    index: Conditionally looped list and dictionary

--- index.py
+++ index.py
@@ -1,3 +1,19 @@
 famous_person = "Albert Einstein"
 message = "\"A person who never made a mistake never tried anything new.\""
 print(f"{famous_person} once said, {message}")
+
+voters = ['alice', 'bob', 'smith']
+favourite_languages = {
+    'alice': 'python',
+    'tyson': 'C#',
+    'Ali': 'Golang'
+}
+if voters:
+    for voter in voters:
+        voter.lower()
+        if voter in favourite_languages:
+            print(f"Dear {voter.title()}, thanks for responding to our poll")
+        else:
+            print(f"Hey {voter.title()}, you're invited to take this poll")
+else:
+    print("Oh no! No voters yet.")
